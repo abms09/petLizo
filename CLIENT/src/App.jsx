ecbe359959567e389230e50abd6fed9e8b47e159
abms09 <binuadarsh40@gmail.com>
2026-04-23 18:37:36 +0530

    changed

--- CLIENT/src/App.jsx
+++ CLIENT/src/App.jsx
@@ -50,7 +50,7 @@ import Chat from "../components/Chat";
 
 function AppWrapper() {
   return (
-    <BrowserRouter basename="/petLizo">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   );
